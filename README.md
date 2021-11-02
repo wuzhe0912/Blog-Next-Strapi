9cc529ceafecff8838be0665f84889c296850493
Pitt Wu <kgb00128@gmail.com>
2021-11-02 12:37:04 +0800

    delete: clean old data

--- README.md
+++
@@ -1,3 +0,0 @@
-# European bison
-
-> 整理`Vue(2.x)`業務開發功能
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transpileDependencies: ['vuetify'],
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].title = 'European bison';
-      return args;
-    });
-  },
-};
